d84c28d71394be1ff8d29d4945596045af80ba62
Amin394 <amin394@outlook.com>
2024-02-14 23:35:09 +0100

    added routing to products list

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
 import "./App.css"
+
 import NavigationBar from "./components/NavigationBar/NavigationBar"
 import Filters from "./components/Filters/Filters"
 import Categories from "./components/Categories/Categories"
 import ProductsList from "./components/ProductsList/ProductsList"
 import Cart from "./components/Cart/Cart"
 import CartProvider from "./utils/contexts/CartContext"
+import ProductDetails from "./components/ProductDetails/ProductDetails"
+
 import { useState } from "react"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   const [cartShown, updateCartShown] = useState(false)
   let toggleCartShown = () => { updateCartShown(!cartShown) }
 
   return (
-    <>
-      <CartProvider>
-        <NavigationBar {...{ toggleCartShown }} />
-        <Categories />
-        <div className="mainSection">
-          <Filters />
-          <ProductsList />
-        </div>
-        {cartShown && <Cart />}
-      </CartProvider>
-    </>
+    <CartProvider>
+      <NavigationBar {...{ toggleCartShown }} />
+      <Categories />
+
+      <div className="mainSection">
+        <Routes>
+          <Route path="/" element={<> <Filters /> <ProductsList /> </>} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="*" element={<h1>NOT FOUND</h1>} />
+        </Routes>
+      </div>
+
+      {cartShown && <Cart />}
+    </CartProvider>
+
   )
 }
 
 export default App
+// https://youtu.be/Ul3y1LXxzdU
--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,6 +1,41 @@
+import { useParams } from "react-router-dom"
+import "./ProductDetails.css"
+
 function ProductDetails() {
+  const { id } = useParams()
+
   return (
-    <div>ProductDetails</div>
+    <>
+      <div className="productDetails">
+        <div className="imagesSection">
+          <img className="focusedImage" />
+          <div className="otherImages">
+            <img className="unfocusedImage"></img>
+          </div>
+        </div>
+        <div className="detailsSection">
+          <div className="title"></div>
+          <div className="description"></div>
+          <div className="specifications"></div>
+        </div>
+        <div className="purchaseSection">
+          <div className="priceInfo"></div>
+          <div className="purchaseButtons"></div>
+          <div className="sellerInfo"></div>
+        </div>
+      </div>
+      <div className="similarProducts">
+        <div>PRODUCT {id}</div>
+      </div>
+      <div className="productReviews">
+        <div className="overallScore"></div>
+        <div className="commentsSection">
+          <div className="comment">
+            <input className="userScore"></input>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
